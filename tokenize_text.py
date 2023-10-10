c80ccb648d31f2840a250a24ff5767db62c94fc6
shigureTufts <ju-hung.chen@tufts.edu>
2023-10-10 15:20:37 -0400

    test commit

---
+++ tokenize_text.py
@@ -0,0 +1,25 @@
+def tokenize_text(raw_text):
+    ''' Transform a plain-text string into a list of tokens
+
+    We assume that *whitespace* divides tokens.
+
+    Args
+    ----
+    raw_text : string
+
+    Returns
+    -------
+    list_of_tokens : list of strings
+        Each element is one token in the provided text
+    '''
+    list_of_tokens = raw_text.split()  # split method divides on whitespace by default
+    for pp in range(len(list_of_tokens)):
+        cur_token = list_of_tokens[pp]
+        # Remove punctuation
+        for punc in ['?', '!', '_', '.', ',', '"', '/']:
+            cur_token = cur_token.replace(punc, "")
+        # Turn to lower case
+        clean_token = cur_token.lower()
+        # Replace the cleaned token into the original list
+        list_of_tokens[pp] = clean_token
+    return list_of_tokens
