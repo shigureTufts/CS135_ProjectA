3cd875b3862fdf70614643972b6bc4b05f5bdd9c
Arman <armanlogins@gmail.com>
2023-10-11 23:00:36 -0400

    Initial commit

---
+++ new_main.py
@@ -0,0 +1,52 @@
+import os
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+import sklearn.pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+
+data_dir = 'data_reviews'
+x_train_df = pd.read_csv(os.path.join(data_dir, 'x_train.csv'))
+y_train_df = pd.read_csv(os.path.join(data_dir, 'y_train.csv'))
+
+N, n_cols = x_train_df.shape
+print("Shape of x_train_df: (%d, %d)" % (N, n_cols))
+print("Shape of y_train_df: %s" % str(y_train_df.shape))
+
+# Print out the first five rows and last five rows
+tr_text_list = x_train_df['text'].values.tolist()
+
+
+
+vocab_list = ['good', 'great', 'like', 'bad', 'best', 'love', 'excellent', 'better', 'recommend',
+              'nice', 'disappointed', 'pretty', 'worst', 'waste', 'amazing', 'terrible', 'wonderful',
+              'poor', 'friendly', 'loved', 'delicious', 'horrible', 'cool', 'happy', 'awesome', 'awful',
+              'stupid', 'perfect', 'impressed', 'comfortable', 'fantastic', 'beautiful', 'interesting',
+              'perfectly', 'disappointing', 'super', 'fast', 'problem', 'bland', 'worse', 'enjoyed', 'fresh',
+              'avoid', 'incredible', 'didn\'t work', 'weird', 'useless']
+
+vocab_dict = dict()
+
+for vocab_id, tok in enumerate(vocab_list):
+    vocab_dict[tok] = vocab_id
+
+bow_preprocessor = CountVectorizer(binary=False, vocabulary=vocab_dict)
+
+bow_preprocessor.fit(tr_text_list)
+
+sparse_arr = bow_preprocessor.transform(tr_text_list)
+
+print(sparse_arr.shape)
+
+dense_arr_NV = sparse_arr.toarray()
+
+print(dense_arr_NV.shape)
+
+print(dense_arr_NV)
+
+bow_preprocessor = CountVectorizer(ngram_range=(1,1), min_df=1, max_df=1.0, binary=False)
+
+bow_preprocessor.fit(tr_text_list)
+
+for term, count in list(bow_preprocessor.vocabulary_.items())[:10]:
+    print("%4d %s" % (count, term))
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import os
+import sklearn.linear_model
+import sklearn.pipeline
 
 data_dir = 'data_reviews'
 x_train_df = pd.read_csv(os.path.join(data_dir, 'x_train.csv'))
@@ -53,11 +55,11 @@ def tokenize_text(raw_text):
 
 tr_text_array = x_train_df['text']
 
-for line in tr_text_array:
-    print("\nRaw text:")
-    print(line)
-    print("Clean token list:")
-    print(tokenize_text(line))
+# for line in tr_text_array:
+#     print("\nRaw text:")
+#     print(line)
+#     print("Clean token list:")
+#     print(tokenize_text(line))
 
 tok_count_dict = dict()
 
@@ -77,14 +79,24 @@ sorted_tokens = list(sorted(tok_count_dict, key=tok_count_dict.get, reverse=True
 # for w in sorted_tokens[-10:]:
 #     print("%5d %s" % (tok_count_dict[w], w))
 
-vocab_list = [w for w in sorted_tokens if tok_count_dict[w] >= 4]
+# vocab_list = [w for w in sorted_tokens if tok_count_dict[w] >= 4]
 
 # for w in vocab_list:
 #     print("%5d %s" % (tok_count_dict[w], w))
 
+vocab_list = ['good', 'great', 'like', 'bad', 'best', 'love', 'excellent', 'better', 'recommend',
+              'nice', 'disappointed', 'pretty', 'worst', 'waste', 'amazing', 'terrible', 'wonderful',
+              'poor', 'friendly', 'loved', 'delicious', 'horrible', 'cool', 'happy', 'awesome', 'awful',
+              'stupid', 'perfect', 'impressed', 'comfortable', 'fantastic', 'beautiful', 'interesting',
+              'perfectly', 'disappointing', 'super', 'fast', 'problem', 'bland', 'worse', 'enjoyed', 'fresh',
+              'avoid', 'incredible', 'didn\'t work', 'weird', 'useless']
+
 vocab_dict = dict()
+
 for vocab_id, tok in enumerate(vocab_list):
     vocab_dict[tok] = vocab_id
+
+
 
 
 def transform_text_into_feature_vector(text, vocab_dict):
@@ -113,4 +125,40 @@ def transform_text_into_feature_vector(text, vocab_dict):
     return count_V
 
 
-print(transform_text_into_feature_vector("the was this the of a an of a", vocab_dict))
+# print(transform_text_into_feature_vector("the was this the of a an of a", vocab_dict))
+
+# for line in tr_text_array:
+#     print("\nRaw text:")
+#     print(line)
+#     print("Clean token list:")
+#     print(transform_text_into_feature_vector(line, vocab_dict))
+
+V = len(vocab_dict)
+x_tr_NV = np.zeros((N, V))
+
+for nn, raw_text_line in enumerate(tr_text_array):
+    x_tr_NV[nn] = transform_text_into_feature_vector(raw_text_line, vocab_dict)
+
+# print(x_tr_NV.shape)
+#
+# print(np.sum(x_tr_NV[:,0]))
+
+y_true = np.ravel(y_train_df)
+
+
+clf = sklearn.linear_model.LogisticRegression(
+    C=1000.0, max_iter=1000)
+
+clf.fit(x_tr_NV, y_true)
+
+yhat_tr_N = clf.predict(x_tr_NV)
+acc = np.mean( y_true == yhat_tr_N )
+
+print("Training accuracy: %.3f" % acc)
+
+
+weights_V = clf.coef_[0]
+sorted_tok_ids_V = np.argsort(weights_V)
+
+for vv in sorted_tok_ids_V:
+    print("% 7.3f %s" % (weights_V[vv], vocab_list[vv]))
