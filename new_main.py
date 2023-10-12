6f1afbaed9cf4d7001209038cf6232657eab7481
Arman <armanlogins@gmail.com>
2023-10-12 16:40:17 -0400

    Initial commit

--- new_main.py
+++ new_main.py
@@ -5,8 +5,12 @@ import sklearn.linear_model
 import sklearn.pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', 10)
+
 data_dir = 'data_reviews'
 x_train_df = pd.read_csv(os.path.join(data_dir, 'x_train.csv'))
+x_test_df = pd.read_csv(os.path.join(data_dir, 'x_test.csv'))
 y_train_df = pd.read_csv(os.path.join(data_dir, 'y_train.csv'))
 
 N, n_cols = x_train_df.shape
@@ -16,14 +20,14 @@ print("Shape of y_train_df: %s" % str(y_train_df.shape))
 # Print out the first five rows and last five rows
 tr_text_list = x_train_df['text'].values.tolist()
 
-
+test_text_list = x_test_df['text'].values.tolist()
 
 vocab_list = ['good', 'great', 'like', 'bad', 'best', 'love', 'excellent', 'better', 'recommend',
               'nice', 'disappointed', 'pretty', 'worst', 'waste', 'amazing', 'terrible', 'wonderful',
               'poor', 'friendly', 'loved', 'delicious', 'horrible', 'cool', 'happy', 'awesome', 'awful',
               'stupid', 'perfect', 'impressed', 'comfortable', 'fantastic', 'beautiful', 'interesting',
               'perfectly', 'disappointing', 'super', 'fast', 'problem', 'bland', 'worse', 'enjoyed', 'fresh',
-              'avoid', 'incredible', 'didn\'t work', 'weird', 'useless']
+              'avoid', 'incredible', 'didn\'t work', 'weird', 'useless', 'enjoy']
 
 vocab_dict = dict()
 
@@ -42,11 +46,74 @@ dense_arr_NV = sparse_arr.toarray()
 
 print(dense_arr_NV.shape)
 
-print(dense_arr_NV)
+# print(dense_arr_NV)
 
-bow_preprocessor = CountVectorizer(ngram_range=(1,1), min_df=1, max_df=1.0, binary=False)
+# bow_preprocessor = CountVectorizer(ngram_range=(1,1), min_df=1, max_df=1.0, binary=False)
+#
+# bow_preprocessor.fit(tr_text_list)
 
-bow_preprocessor.fit(tr_text_list)
+# for term, count in list(bow_preprocessor.vocabulary_.items())[:10]:
+#     print("%4d %s" % (count, term))
 
-for term, count in list(bow_preprocessor.vocabulary_.items())[:10]:
-    print("%4d %s" % (count, term))
+
+# print(len(bow_preprocessor.vocabulary_))
+
+my_bow_classifier_pipeline = sklearn.pipeline.Pipeline([
+    ('my_bow_feature_extractor', CountVectorizer(min_df=1, max_df=1.0, ngram_range=(1, 1), vocabulary=vocab_dict, binary=False)),
+    ('my_classifier', sklearn.linear_model.LogisticRegression(C=1.0, max_iter=100, random_state=101))
+])
+
+my_parameter_grid_by_name = dict()
+my_parameter_grid_by_name['my_bow_feature_extractor__min_df'] = [1, 2, 4]
+my_parameter_grid_by_name['my_classifier__C'] = np.logspace(-4, 4, 9)
+
+my_scoring_metric_name = 'accuracy'
+y_true = np.ravel(y_train_df)
+# print(y_true)
+
+prng = np.random.RandomState(0)
+
+valid_ids = prng.choice(np.arange(N), size=800)
+
+valid_indicators_N = np.zeros(N)
+valid_indicators_N[valid_ids] = -1
+
+my_splitter = sklearn.model_selection.PredefinedSplit(valid_indicators_N)
+
+grid_searcher = sklearn.model_selection.GridSearchCV(
+    my_bow_classifier_pipeline,
+    my_parameter_grid_by_name,
+    scoring=my_scoring_metric_name,
+    cv=my_splitter,
+    refit=False)
+
+grid_searcher.fit(tr_text_list, y_true)
+
+gsearch_results_df = pd.DataFrame(grid_searcher.cv_results_).copy()
+
+param_keys = ['param_my_bow_feature_extractor__min_df', 'param_my_classifier__C']
+
+# Rearrange row order so it is easy to skim
+gsearch_results_df.sort_values(param_keys, inplace=True)
+
+var = gsearch_results_df[param_keys + ['split0_test_score', 'rank_test_score']]
+
+print(var)
+
+# y_test_pred = np.ravel(test_text_list)
+# my_bow_classifier_pipeline.fit(tr_text_list, y_true)
+# result = my_bow_classifier_pipeline.predict(y_test_pred)
+#
+# # print(result[:, np.newaxis])
+
+
+my_bow_classifier_pipeline.fit(tr_text_list, y_true)
+
+
+
+yhat_tr_N = my_bow_classifier_pipeline.predict(tr_text_list)
+
+# print(yhat_tr_N[:,np.newaxis])
+
+acc = np.mean(y_true == yhat_tr_N)
+print(acc)
---
+++ test_2.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+import sklearn.pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+
+data_dir = 'data_reviews'
+x_train_df = pd.read_csv(os.path.join(data_dir, 'x_train.csv'))
+x_test_df = pd.read_csv(os.path.join(data_dir, 'x_test.csv'))
+y_train_df = pd.read_csv(os.path.join(data_dir, 'y_train.csv'))
+
+N, n_cols = x_train_df.shape
+
+tr_text_list = x_train_df['text'].values.tolist()
+
+test_text_list = x_test_df['text'].values.tolist()
+
+vocab_list = ['good', 'great', 'like', 'bad', 'best', 'love', 'excellent', 'better', 'recommend',
+              'nice', 'disappointed', 'pretty', 'worst', 'waste', 'amazing', 'terrible', 'wonderful',
+              'poor', 'friendly', 'loved', 'delicious', 'horrible', 'cool', 'happy', 'awesome', 'awful',
+              'stupid', 'perfect', 'impressed', 'comfortable', 'fantastic', 'beautiful', 'interesting',
+              'perfectly', 'disappointing', 'super', 'fast', 'problem', 'bland', 'worse', 'enjoyed', 'fresh',
+              'avoid', 'incredible', 'didn\'t work', 'weird', 'useless', 'enjoy']
+
+vocab_dict = dict()
+
+for vocab_id, tok in enumerate(vocab_list):
+    vocab_dict[tok] = vocab_id
+
+###############################################################################################333
+
+prng = np.random.RandomState(0)
+valid_ids = prng.choice(np.arange(N), size=800)
+valid_indicators_N = np.zeros(N)
+valid_indicators_N[valid_ids] = -1
+my_splitter = sklearn.model_selection.PredefinedSplit(valid_indicators_N)
+
+my_bow_classifier_pipeline = sklearn.pipeline.Pipeline([
+    ('my_bow_feature_extractor', CountVectorizer(min_df=4, max_df=1.0, ngram_range=(1, 1),
+                                                 vocabulary=vocab_dict, binary=False)),
+    ('my_classifier', sklearn.linear_model.LogisticRegression(C=10000.0, max_iter=100, random_state=101)),
+    # ('my_splitter', sklearn.model_selection.PredefinedSplit(valid_indicators_N)),
+    # ('grid_searcher', sklearn.model_selection.GridSearchCV(np.logspace(-4, 4, 9), [1, 2, 3], refit=False))
+])
+
+y_true = np.ravel(y_train_df)
+my_bow_classifier_pipeline.fit(tr_text_list, y_true)
+
+yhat_tr_N = my_bow_classifier_pipeline.predict(tr_text_list)
+
+# print(yhat_tr_N[:,np.newaxis])
+
+acc = np.mean(y_true == yhat_tr_N)
+print(acc)
